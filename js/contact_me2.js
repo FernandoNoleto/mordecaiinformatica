69d790ef5f0848d56a50ac68975b144fad952be8
FernandoNoleto <fernandoleto@hotmail.com>
2020-08-05 11:23:29 -0300

    commit inicial

---
+++ js/contact_me2.js
@@ -0,0 +1,6 @@
+document.getElementById('contactForm').addEventListener('submit', function () {
+    var nome = this.querySelector('input[name=nome]'), nome = nome.value;
+    var email = this.querySelector('input[name=email]'), email = email.value;
+    var texto = 'Olá destinatário, \nMeu nome é '+ nome +' e meu email é '+ email;
+    this.querySelector('input[name=Body]').setAttribute('value', texto);
+});
